3388b78291406eb555c671e65ea230ae78f2d2c3
swmu <swmu@foxmail.com>
2017-12-27 00:28:16 +0800

    构建博客架构

--- SwainBlog/frontend/src/views/manage/manage-home.vue
+++ SwainBlog/frontend/src/views/manage/manage-home.vue
@@ -1,11 +1,41 @@
 <template>
   <div class="manage-home-wrapper">
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">内容管理</template>
+            <el-menu-item index="2-1">文章管理</el-menu-item>
+            <el-menu-item index="2-2">今日事</el-menu-item>
+            <el-menu-item index="2-3">其他</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="3">个人中心</el-menu-item>
+        </el-menu>
+      </el-header>
+
+
       <el-main>Main</el-main>
     </el-container>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
 
 <style>
   .manage-home-wrapper{
@@ -15,7 +45,8 @@
     flex-direction: column;
   }
   .el-header{
-    background-color: antiquewhite;
+    /*padding: 0px;*/
+    /*background-color: antiquewhite;*/
   }
   .el-main{
     background-color: aquamarine;
